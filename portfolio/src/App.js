c8da4225720099fa70aa2d4e1798b473f1ce1ffa
rakesh-ganganapalli-au13 <rakeshganganapalli72>
2021-05-19 12:41:07 +0530

    navbar

---
+++ portfolio/src/App.js
@@ -0,0 +1,13 @@
+import Navbar  from './components/navbar/navbar';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <Navbar></Navbar>
+    </div>
+  );
+}
+
+
+export default App;
